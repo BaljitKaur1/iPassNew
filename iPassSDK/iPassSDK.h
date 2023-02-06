0025eb0725d6b776f22633ec7d36c08444f81e76
BaljitKaur1 <baljitkaur@csgroupchd.com>
2023-02-06 15:05:22 +0530

    First commit

---
+++ iPassSDK/iPassSDK.h
@@ -0,0 +1,18 @@
+//
+//  iPassSDK.h
+//  iPassSDK
+//
+//  Created by Mobile on 06/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iPassSDK.
+FOUNDATION_EXPORT double iPassSDKVersionNumber;
+
+//! Project version string for iPassSDK.
+FOUNDATION_EXPORT const unsigned char iPassSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iPassSDK/PublicHeader.h>
+
+
