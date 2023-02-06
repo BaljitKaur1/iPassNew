1a1001449a3c244afd460355e1513e77ca1185be
BaljitKaur1 <baljitkaur@csgroupchd.com>
2023-02-06 15:36:37 +0530

    Added podspec

---
+++ iPassSDK/iPassSDK.docc/Resources/TestFile.swift
@@ -0,0 +1,12 @@
+//  TestFile.swift
+//  Created by Mobile on 06/02/23.
+
+import Foundation
+import UIKit
+
+class TestVC : UIViewController {
+    override func viewDidLoad() {
+        print("21212")
+        asas
+    }
+}
